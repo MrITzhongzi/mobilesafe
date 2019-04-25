93497448b436c18613ede8d676aff9f758791b4a
李洪伟 <17862806857@163.com>
2019-04-25 17:32:57 +0800

    更换项目maven镜像，报警定位功能

--- app/src/main/java/com/example/www/service/LocationService.java
+++ app/src/main/java/com/example/www/service/LocationService.java
@@ -1,9 +1,17 @@
 package com.example.www.service;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
 
 public class LocationService extends Service {
     public LocationService() {
@@ -15,7 +23,43 @@ public class LocationService extends Service {
         // 获取手机经纬度坐标
         LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
         // 以最优的方式获取经纬度坐标
+        Criteria criteria = new Criteria();
+        criteria.setCostAllowed(true);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String bestProvider = lm.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        MyLocationListener myLocationListener = new MyLocationListener();
+        lm.requestLocationUpdates(bestProvider, 0, 0, myLocationListener);
 
+    }
+
+    class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+
+            SmsManager aDefault = SmsManager.getDefault();
+            aDefault.sendTextMessage("15555215554",null, "longitude" + longitude + "----latitude" + latitude, null, null);
+            stopSelf();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
     @Override
