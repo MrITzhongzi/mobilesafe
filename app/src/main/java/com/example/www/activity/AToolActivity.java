e7d8af9695ac30433d5d2094160c49294b5518b0
李洪伟 <17862806857@163.com>
2019-05-10 12:01:02 +0800

    短信备份

--- app/src/main/java/com/example/www/activity/AToolActivity.java
+++ app/src/main/java/com/example/www/activity/AToolActivity.java
@@ -1,26 +1,85 @@
 package com.example.www.activity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.www.engine.SmsBackUp;
 import com.example.www.mobilesafe.R;
+
+import java.io.File;
 
 public class AToolActivity extends AppCompatActivity {
 
     private TextView mTv_query_add;
+    private TextView mTv_sms_backup;
+    private ProgressBar mSmsProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atool);
 
-        //电话归属地查询
-        initPhoneAddress();
+        requestPermissions(new String[]{Manifest.permission.READ_SMS}, 100);
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 100) {
+            //电话归属地查询
+            initPhoneAddress();
+            //短信备份方法
+            initSmsBackup();
+        }
+    }
+
+    private void initSmsBackup() {
+        mTv_sms_backup = (TextView) findViewById(R.id.tv_sms_backup);
+        mTv_sms_backup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSmsBackUpDialog();
+            }
+
+        });
+    }
+
+    private void showSmsBackUpDialog() {
+        mSmsProgressBar = (ProgressBar) findViewById(R.id.pb_sms_backup_progress);
+        mSmsProgressBar.setVisibility(View.VISIBLE);
+
+        new Thread(){
+            @Override
+            public void run() {
+                String smsPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "sms.xml";
+                SmsBackUp.backup(getApplicationContext(), smsPath, new SmsBackUp.CallBack() {
+                    @Override
+                    public void setMax(int max) {
+                        mSmsProgressBar.setMax(max);
+                    }
+
+                    @Override
+                    public void setProgress(int index) {
+                        mSmsProgressBar.setProgress(index);
+                    }
+
+                    @Override
+                    public void close() {
+                        mSmsProgressBar.setVisibility(View.GONE);
+                    }
+                });
+            }
+        }.start();
+    }
     private void initPhoneAddress() {
         mTv_query_add = (TextView) findViewById(R.id.tv_query_add);
         mTv_query_add.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/www/engine/SmsBackUp.java
@@ -0,0 +1,105 @@
+package com.example.www.engine;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Xml;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class SmsBackUp {
+
+    private static Handler mHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            if(index < count) {
+                callBack.setProgress(index);
+            }else {
+//                callBack.setVisibility(View.GONE);
+                index = 0;
+            }
+        }
+    };
+    private static int index = 0;
+    private static CallBack callBack;
+    private static int count;
+
+    public static void backup(Context ctx, String path, CallBack callBack) {
+        SmsBackUp.callBack = callBack;
+        //需要的对象： 上下文环境， 备份文件夹的路径， 进度条的对象（需要不断变化），
+        try {
+            // 1获取备份短信写入的文件
+            File file = new File(path);
+            // 获取内容解析器.
+            Cursor cursor = ctx.getContentResolver().query(Uri.parse("content://sms/"), new String[]{"address", "date", "type", "body"}, null, null, null);
+
+            count = cursor.getCount();
+//            pb.setMax(count);
+            callBack.setMax(count);
+
+            FileOutputStream fos = new FileOutputStream(file);
+            XmlSerializer xmlSerializer = Xml.newSerializer();
+            xmlSerializer.setOutput(fos, "utf-8");
+            // 独立的规范
+            xmlSerializer.startDocument("utf-8", true);
+            xmlSerializer.startTag(null, "smss");
+            while (cursor.moveToNext()){
+                xmlSerializer.startTag(null, "sms");
+
+                xmlSerializer.startTag(null, "address");
+                xmlSerializer.text(cursor.getString(0));
+                xmlSerializer.endTag(null, "address");
+
+                xmlSerializer.startTag(null, "date");
+                xmlSerializer.text(cursor.getString(1));
+                xmlSerializer.endTag(null, "date");
+
+                xmlSerializer.startTag(null, "type");
+                xmlSerializer.text(cursor.getString(2));
+                xmlSerializer.endTag(null, "type");
+
+                xmlSerializer.startTag(null, "body");
+                xmlSerializer.text(cursor.getString(3));
+                xmlSerializer.endTag(null, "body");
+
+                xmlSerializer.endTag(null, "sms");
+
+                index++;
+
+                Thread.sleep(1000);
+                SmsBackUp.mHandler.sendEmptyMessage(0);
+            }
+            xmlSerializer.endTag(null, "smss");
+            xmlSerializer.endDocument();
+
+            SmsBackUp.mHandler.sendEmptyMessage(0);
+
+            cursor.close();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public interface CallBack{
+        /***
+         * 短信总数
+         * @param max
+         */
+        public void setMax(int max);
+
+        public void setProgress(int index);
+
+        public void close();
+
+    }
+}
