eb53751e37e813948d7a1f74222ec6971e467f9f
李洪伟 <17862806857@163.com>
2019-05-21 11:05:32 +0800

    全局捕获异常

--- app/src/main/java/com/example/www/activity/MyApplication.java
+++ app/src/main/java/com/example/www/activity/MyApplication.java
@@ -1,11 +1,13 @@
 package com.example.www.activity;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
 public class MyApplication extends Application {
+    static  final String TAG = "MyApplication";
     // 在application的onCreate中初始化
     @Override
     public void onCreate() {
@@ -13,5 +15,14 @@ public class MyApplication extends Application {
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
 
+
+        //全局捕获 未捕获的异常
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                e.printStackTrace();
+                Log.i(TAG, "捕获到了一个程序的异常");
+            }
+        });
     }
 }
